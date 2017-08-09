ccaaad20c22c6867e23b0d957ce018fae7d0db70
Rui Deyllot <rui.deyllot@gmail.com>
2017-08-09 22:44:46 +0100

    Created patch to use sand from the Fertile Fields mod instead of rock chunks.

---
+++ Glass&Lights/Source/PatchOperationIsModActive/PatchOperationIsModActive/PatchOperationIsModActive.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Xml;
+using Verse;
+
+namespace PatchOperationIsModActive
+{
+    internal class PatchOperationIsModActive : PatchOperation
+    {
+        private string mod;
+
+        protected override bool ApplyWorker(XmlDocument xml)
+        {
+            if(mod.NullOrEmpty())
+            {
+                return false;
+            }
+
+            return ModsConfig.ActiveModsInLoadOrder.Any(activeMod => activeMod.Name == mod);
+        }
+    }
+}
